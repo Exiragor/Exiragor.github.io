678b4c350ea1feb2f660a4d5faeaab4f4102c9ca
a.m.lebedev <a.m.lebedev@tinkoff.ru>
2023-06-22 12:21:10 +0300

    feat: add first page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h2>Lebedev Aleksander - frontend</h2>
+<main>
+    <h1>Lebedev Aleksander - frontend</h1>
+</main>
+
+<style>
+    main {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: end;
+        background-color: black;
+        color: white;
+    }
+</style>
+
