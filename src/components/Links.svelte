5877ea684c4a5e92f9cb8e3e905d76bdb34b521a
a.m.lebedev <a.m.lebedev@tinkoff.ru>
2023-07-19 15:30:27 +0300

    feat: skills & some refactor

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -1,7 +1,7 @@
 <script>
     import {links} from '../consts'
     export let linkStyle = 'w-10 md:w-14';
-    export let blockWidth = '3/4';
+    export let blockWidth = '9/12';
 </script>
 
 <div class="flex justify-between w-{blockWidth}">
--- src/components/Skill.svelte
+++ src/components/Skill.svelte
@@ -1,12 +1,19 @@
 <script>
-    import {Progressbar} from "flowbite-svelte";
-
     export let name = '';
     export let progress = '100';
+
+    let width = '0';
+
+    // animate progress usage
+    setTimeout(() => {
+        width = progress;
+    });
 </script>
 
 <div class="flex flex-col">
     <span class="ml-2 text-sm font-medium">{name}</span>
-    <Progressbar class="bg-neutral-300" progress={progress} size="h-5" labelInsideClass="bg-orange-400"/>
-</div>
 
+    <div class="w-full rounded-full dark:bg-gray-700 h-5 bg-neutral-300">
+        <div class="bg-primary-600 h-5 rounded-full transition-all ease-in-out duration-700" style="width: {width}%;"></div>
+    </div>
+</div>
