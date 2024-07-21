2f9fad433aedc46057506b9cca4a5bcd1ba1cc82
a.m.lebedev <a.m.lebedev@tbank.ru>
2024-07-21 15:33:17 +0300

    fix: change margin top style

--- src/components/timeline/TimeLineItem.svelte
+++ src/components/timeline/TimeLineItem.svelte
@@ -1,10 +1,4 @@
-<script>
-	export let marginTop = '5';
-</script>
-
 <li class="mb-10 ms-4">
-	<div
-		class="absolute w-3 h-3 bg-orange-400 rounded-full mt-{marginTop} -start-1.5 border border-white"
-	/>
+	<div class="absolute w-3 h-3 bg-orange-400 rounded-full mt-5 -start-1.5 border border-white" />
 	<slot />
 </li>
