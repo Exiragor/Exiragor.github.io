cc7e40e1ea8964a305a5fb651888ea958284dba1
a.m.lebedev <a.m.lebedev@tbank.ru>
2024-07-21 15:55:09 +0300

    fix: some text issues

--- src/components/sections/About.svelte
+++ src/components/sections/About.svelte
@@ -13,9 +13,9 @@
 				<div class="flex flex-col gap-y-2">
 					<h2 class="text-2xl font-bold text-white text-center md:text-left">About me</h2>
 					<p class="prose prose-sm text-gray-300 sm:prose-base">
-						As you might guess, I love programming :) I started to get involved in development since
-						school, initially attracted backend, but then moved to the frontend. Sometimes I try
-						myself in different technologies on pet projects. In my free time I enjoy chess,
+						As you might guess, I love programming :) I started developing since high school, at
+						first I was attracted to the backend, but then I switched to the front-end. Sometimes I
+						try myself in different technologies on pet projects. In my free time I enjoy chess,
 						travelling, movies and coffee. I periodically install Linux for myself until I burn out
 						from it. I dreamed of getting into a cool team and company, so I ended up in T-Bank crew
 						as a front-end developer and like it!
